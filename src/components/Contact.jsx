2ff81d2917001f752bb980e2fb07a5ee04aba094
stravo1@msi <stravoone@gmail.com>
2023-01-30 00:19:41 +0530

    build: contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,73 +1,70 @@
 import { useState } from "preact/hooks";
 
 export default function Modal() {
-  const [visible, setVisible] = useState(false);
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [message, setMessage] = useState("");
 
   return (
     <>
-      <button
-        onClick={() => {
-          setVisible(true);
-        }}
-      >
-        Contact
-      </button>
-      <div
-        
-        class="fixed top-0 left-0 h-screen w-screen z-50 flex items-center justify-center bg-black bg-opacity-75"
-        style={visible ? { display: "flex" } : { display: "none" }}
-      >
-        <div class="bg-orange-100 rounded-lg p-4 md:p-10 text-amber-900 w-[75vw]">
-          <form>
-            <h2 class="font-bold text-xl mb-4">Contact Us</h2>
-            <div class="mb-4">
-              <label class="bloc font-medium mb-2" for="name">
-                Name
-              </label>
-              <input
-                class="border border-amber-200 bg-orange-50 p-2 rounded-lg w-full"
-                type="text"
-                id="name"
-                name="name"
-                required
-              />
-            </div>
-            <div class="mb-4">
-              <label class="block font-medium mb-2" for="email">
-                Email
-              </label>
-              <input
-                class="border border-amber-200 bg-orange-50 p-2 rounded-lg w-full"
-                type="email"
-                id="email"
-                name="email"
-                required
-              />
-            </div>
-            <div class="mb-4">
-              <label class="block font-medium mb-2" for="message">
-                Message
-              </label>
-              <textarea
-                class="border border-amber-200 bg-orange-50 p-2 rounded-lg w-full h-32"
-                id="message"
-                name="message"
-                required
-              ></textarea>
-            </div>
-          </form>
-
-          <div class="flex justify-end">
-            <button
-              onClick={() => {
-                setVisible(false);
-              }}
-              class="bg-amber-900 w-full rounded-lg p-4 text-amber-200 my-4"
-            >
-              Send
-            </button>
-          </div>
+      <div className="p-2 text-amber-900">
+        <div className="mb-4">
+          <label className="block font-medium mb-2" for="name">
+            Name *
+          </label>
+          <input
+            className="w-full border border-amber-700 p-2 rounded-lg"
+            type="text"
+            id="name"
+            name="name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2" for="email">
+            Phone Number *
+          </label>
+          <input
+            className="w-full border border-amber-700 focus:border-amber-900 focus:border-0 p-2 rounded-lg"
+            type="tel"
+            id="tel"
+            name="tel"
+            required
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2" for="message">
+            Message
+          </label>
+          <textarea
+            className="w-full border border-amber-700 p-2 rounded-lg"
+            id="message"
+            name="message"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <a
+          onClick={(e) => {
+            if (!(name && number)) {
+              alert("Fill in all the details!");
+              console.log(name);
+              e.preventDefault();
+            }
+          }}
+          href={`mailto:goldspringsofficial@gmail.com?subject=Hello Gold Springs!&body=Hi, I ${name} would like to discuss a few things with you guys! \n${
+            message ? `Here's a short description: ${message}.` : ""
+          }\n Here's my phone number: ${number}.`}
+        >
+          <button className="bg-amber-900 w-full lg:w-60 rounded-lg p-4 text-amber-200 my-4 font-bold">
+            Send a mail
+          </button>
+        </a>
       </div>
     </>
   );
